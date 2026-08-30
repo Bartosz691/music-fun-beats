c788efa7c8e6d5e31731cb6610dd0b9d5ff095f3
Bartosz Wypych <ccgalerty789@gmail.com>
2026-08-30 17:10:33 +0200

    feat(music-fun-beats): add custom user model

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +136,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+AUTH_USER_MODEL = 'users.User'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
