36c08c17e5204172000a1c496f111ebff393827c
Bartosz Wypych <ccgalerty789@gmail.com>
2026-09-07 23:52:04 +0200

    feat(music-fun-beats): add Redis caching

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DJANGO_DEBUG=(bool, False),
     USE_SQLITE=(bool, True),
+    USE_REDIS_CACHE=(bool, False),
 )
 
 environ.Env.read_env(BASE_DIR / '.env')
@@ -123,6 +124,37 @@ else:
                 'POSTGRES_PORT',
                 default='5432',
             ),
+        }
+    }
+    
+    USE_REDIS_CACHE = env.bool(
+    'USE_REDIS_CACHE',
+    default=False,
+)
+    
+USE_REDIS_CACHE = env.bool(
+    'USE_REDIS_CACHE',
+    default=False,
+)
+
+if USE_REDIS_CACHE:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': env(
+                'REDIS_CACHE_URL',
+                default='redis://redis:6379/1',
+            ),
+            'OPTIONS': {
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            },
+        }
+    }
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+            'LOCATION': 'music-fun-beats-local-cache',
         }
     }
 
